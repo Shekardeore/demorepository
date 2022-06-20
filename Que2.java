7d595af86aea49a357961c03eecb2339a73505e3
Shekardeore <107829699+Shekardeore@users.noreply.github.com>
2022-06-20 17:15:36 +0530

    Add files via upload

---
+++ Que2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Que2 {
+	public static void main(String[] args)
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+		int i =s.nextInt();
+		int j=s.nextInt();
+		System.out.println("The addition is= "+(i+j));
+	}
+}
---
+++ Que3.java
@@ -0,0 +1,10 @@
+
+public class Que3 {
+
+	public static void main(String[] args) {
+         int a=50;
+         int b=3;
+         int res=a/b;
+         System.out.println("The divide o/p is= "+res);
+	}
+}
